9d4790d6b15137feba2bf8791bdb2c9c01251f1c
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-08-19 20:55:07 +0900

    redis 재시도

--- back/app.js
+++ back/app.js
@@ -4,21 +4,29 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const dotenv = require('dotenv');
+const helmet = require('helmet');
+const hpp = require('hpp');
 const morgan = require('morgan');
 const path = require('path');
-const hpp = require('hpp');
-const helmet = require('helmet');
+const logger = require('./logger');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 
+dotenv.config();
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
 const userRouter = require('./routes/user');
 const hashtagRouter = require('./routes/hashtag');
-const db = require('./models');
+const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
-dotenv.config();
 const app = express();
-db.sequelize.sync()
+app.set('port', process.env.PORT || 3065);
+sequelize.sync({ force: false })
   .then(() => {
     console.log('db 연결 성공');
   })
@@ -27,10 +35,10 @@ passportConfig();
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
-  app.use(hpp());
   app.use(helmet({ contentSecurityPolicy: false }));
+  app.use(hpp());
   app.use(cors({
-    origin: 'http://taewitter.com',
+    origin: ['http://localhost:3000', 'http://taewitter.com'],
     credentials: true,
   }));
 } else {
@@ -44,28 +52,48 @@ app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  saveUninitialized: false,
+const sessionOption = {
   resave: false,
+  saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
     secure: false,
-    domain: process.env.NODE_ENV === 'production' && '.taewitter.com'
+    domain: process.env.NODE_ENV === 'production' && '.taewitter.com '
   },
-}));
+  store: new RedisStore({ client: redisClient }),
+};
+app.use(session(sessionOption));
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.send('hello express');
 });
-
-app.use('/posts', postsRouter);
+// API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use('/post', postRouter);
+app.use('/posts', postsRouter);
 app.use('/user', userRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(80, () => {
+// 404처리 미들웨어
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  logger.info('hello');
+  logger.error(error.message);
+  next(error);
+});
+
+// 에러 처리 미들웨어
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  // 개발 모드일 때는 에러를 보여주게 하고, 배포일 때는 보여주지 않게 하는 코드
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
   console.log('서버 실행 중!');
 });
