37ff9031bf754b2d1fd4c785f72ffeb2de02da9c
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-08-24 23:08:34 +0900

    lambda async 오류 해결

--- lambda/index.js
+++ lambda/index.js
@@ -4,7 +4,7 @@ const sharp = require('sharp');
 const s3 = new AWS.S3();
 
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
     const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
     const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
     console.log(Bucket, Key);
