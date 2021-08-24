186440cbac432359d1bcab6f94abaf15d1c564ce
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-08-24 22:12:39 +0900

    sharp 오타 수정

--- lambda/index.js
+++ lambda/index.js
@@ -1,17 +1,17 @@
 const AWS = require('aws-sdk');
-const sharp = require('shar');
+const sharp = require('sharp');
 
 const s3 = new AWS.S3();
 
 
 exports.handler = (event, context, callback) => {
-    const Bucket = event.Records[0].s3.bucket.name;
-    const Key = decodeURIComponent(event.Records[0].s3.object.key);
+    const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
+    const Key = decodeURIComponent(event.Records[0].s3.object.key); // original/12312312_abc.png
     console.log(Bucket, Key);
     const filename = Key.split('/')[Key.split('/').length - 1];
     const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
     const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
-    console.log('filename', filename);
+    console.log('filename', filename, 'ext', ext);
 
     try {
         const s3Object = await s3.getObject({ Bucket, Key }).promise();
