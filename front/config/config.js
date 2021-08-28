a2fae7cf18119c799f729a82b80b0e35af3adb4f
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-08-28 14:03:12 +0900

    backUrl     경

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
-export const backUrl = 'http://localhost:3065';
-// export const backUrl = 'http://api.taewitter.com';
+export const backUrl = 'http://api.taewitter.com';
+// export const backUrl = 'http://localhost:3065';
